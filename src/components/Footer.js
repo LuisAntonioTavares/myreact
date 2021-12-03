04901864dd0f319e674b714183b1979aa7e23dfc
Luis Tavares <xtluis@gmail.com>
2021-12-03 20:52:55 -0300

    Studying react.

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer(){
-    return (
-        <footer className="foot">
-            <small>© 2021 Tavares development. All rights reserved.</small>
-        </footer>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import MainContent from "./components/MainContent"
+import ReactDOM from 'react-dom'
+import App from './App'
 import "./style.css"
 
-function App(){
-    return (
-        <div>
-            <Header />                     
-            <MainContent />          
-            <Footer/>
-        </div>
-    )
-}
-
-ReactDOM.render(
-    <App />, 
-    document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"))
